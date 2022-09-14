92039dbaa19a1ba6ebd437091447f7a4c8f98652
Pramodreddy0707 <112369481+Pramodreddy0707@users.noreply.github.com>
2022-09-14 10:59:51 +0530

    Delete factorial of no usinf reccursion.py

--- factorial of no usinf reccursion.py
+++
@@ -1,8 +0,0 @@
-num=input("enter the value=")
-if(num.isnumeric()):
-    fact=1
-    for i in range(1,int(num)+1):
-        fact=fact*i
-    print("factorial is",fact)
-else:
-    print("invalid input")
